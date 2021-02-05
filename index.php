033d0dc1765221bc8b8e2ef09903f4173cb8c212
AndrewZales <amz0018@mix.wvu.edu>
2021-02-05 18:26:14 -0500

    lab2

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+  <Link rel="stylesheet" href="css/about.css">
+
+  <div class="about">
+  <h1>About Us</h1>
+
+  </div>
+
+<div class="column">
+  <div class="card">
+  <img src="images/1.jpeg">
+  <div class="containter">
+  <h2>CEO</h2>
+  <p>spiderMan</p>
+  <p>Email:SMan@cs230.com</p>
+  </div>
+  </div>
+</div> 
+<div class="column">
+  <div class="card">
+  <img src="images/2.jpeg">
+  <div class="containter">
+  <h2>CEO</h2>
+  <p>ironman</p>
+  <p>Email:IronMan@cs230.com</p>
+  </div>
+  </div>
+</div> 
+<div class="column">
+  <div class="card">
+  <img src="images/3.jpeg">
+  <div class="containter">
+  <h2>CEO</h2>
+  <p>CaptainAmerica</p>
+  <p>Email:Cap@cs230.com</p>
+  </div>
+  </div>
+</div>
+<div class="column">
+  <div class="card">
+  <img src="images/4.jpeg">
+  <div class="containter">
+  <h2>CEO</h2>
+  <p>Hulk</p>
+  <p>Email:Hulk@cs230.com</p>
+  </div>
+  </div>
+</div>
+  </div>
+</main>
